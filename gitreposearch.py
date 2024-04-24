fa49337369dec0a88619d7d20a18e61323b15eb5
arch3angel <arch3angel@gmail.com>
2024-04-24 15:17:01 -0400

    Commit: Added Search Parameters And Resolved Some Bugs.

--- gitreposearch.py
+++ gitreposearch.py
@@ -31,102 +31,6 @@ if not os.path.exists("templates"):
 if not os.path.exists("search-results"):
     os.makedirs("search-results")
 
-# Function to search GitHub and save markdown
-def search_github_and_save_markdown(search_criteria, github_token):
-    # Initialize variables for pagination
-    per_page = 100  # Maximum items per page for GitHub API
-    page = 1
-    total_repositories = []
-
-    # Report the total number of pages for the search criteria
-    total_pages = get_total_pages(search_criteria, per_page, github_token)
-    st.write(f"#### [{total_pages}] Total Pages For '{search_criteria}'")
-
-    if total_pages == 0:
-        st.write("No Repositories Found For The Given Search Criteria.")
-        return
-
-    while page <= total_pages:
-        # Define GitHub API endpoint for repository search
-        api_url = f"https://api.github.com/search/repositories?q={search_criteria}&per_page={per_page}&page={page}"
-
-        try:
-            # Send GET request to GitHub API
-            response = requests.get(api_url, headers={"Authorization": f"token {github_token}"})
-
-            response.raise_for_status()  # Raise an exception for any other HTTP error
-
-            # Parse JSON response
-            data = response.json()
-
-            # Extract repositories from current page
-            repositories = data.get('items', [])
-
-            if repositories:
-                total_repositories.extend(repositories)
-                st.write(f"Processed Page {page}/{total_pages}")
-            else:
-                break  # Exit the loop if no more repositories on subsequent pages
-
-            page += 1  # Move to the next page for pagination
-
-            time.sleep(1)  # Wait for 1 second before making the next request
-        except requests.exceptions.RequestException as e:
-            st.write(f"Error Occurred: {e}")
-            break
-
-    if total_repositories:
-        # Create Markdown content
-        markdown_content = f"---\n"
-        markdown_content += f"### [ {search_criteria} ]\n"
-        markdown_content += f"#### Summary\n"
-        markdown_content += f"###### Timestamp [YYYYMMDDHHMMSS]: {current_datetime}\n"  # Add timestamp here
-        markdown_content += f"###### Total Repositories In Your Search: {len(total_repositories)}\n"
-        markdown_content += f"---\n"
-
-        for repo in total_repositories:
-            repo_name = repo.get('full_name', '')
-            repo_url = repo.get('html_url', '')
-            repo_description = repo.get('description', '')
-
-            # Format repository information in Markdown syntax
-            markdown_content += f"##### [{repo_name}]({repo_url})\n"
-            markdown_content += f"###### DESCRIPTION:\n"
-            markdown_content += f"- ###### {repo_description}\n"
-            markdown_content += f"---\n"
-
-        # Save Markdown content to file in the "docs" directory and its subdirectories
-        file_name = f"search-results/{search_criteria}_{current_datetime}.md"
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(markdown_content)
-        st.write(f"Markdown File '{file_name}' With Complete Repository List And Summary Saved Successfully.")
-    else:
-        st.write("No Repositories Found For The Given Search Criteria.")
-
-# Function to get total pages from GitHub API
-def get_total_pages(search_criteria, per_page, github_token):
-    # Define GitHub API endpoint for getting total count of repositories
-    total_count_api_url = f"https://api.github.com/search/repositories?q={search_criteria}&per_page=1"
-
-    try:
-        # Send GET request to GitHub API
-        response = requests.get(total_count_api_url, headers={"Authorization": f"token {github_token}"})
-        response.raise_for_status()  # Raise an exception for any HTTP error
-
-        # Parse JSON response
-        data = response.json()
-
-        # Extract total count of repositories
-        total_count = data.get('total_count', 0)
-
-        # Calculate total pages based on total count and items per page
-        total_pages = math.ceil(total_count / per_page)
-
-        return total_pages
-    except requests.exceptions.RequestException as e:
-        st.write(f"Error Occurred While Getting Total Pages: {e}")
-        return 0
-
 # Home Page
 def home():
     st.image(f"{gitreposearch_logo}", width=gitreposearch_logo_width)
@@ -134,12 +38,18 @@ def home():
     st.markdown("A Streamlit app for searching GitHub repositories based on user criteria.<br>", unsafe_allow_html=True)
     st.sidebar.markdown("<small style='font-size: 0.8em;'>Version: 0.1.0</small>", unsafe_allow_html=True)
 
-    # Create the search bar and search button layout
+    # Create the search bar, radio button group, and search button layout
     search_criteria = st.text_input('Enter Your Search Criteria For GitHub Repositories:')
+    star_range = st.radio('Select Star Range:', ["Most Likes", "0-50 Stars", "50-100 Stars", "100-150 Stars", "150-200 Stars", "200-250 Stars", "250-500 Stars", "Greater Than 500 Stars"])
+    include_forks = st.checkbox('Do Not Include Forked Repositories')
     search_button = st.button('Search GitHub')
+
     # Check if the search button is clicked
     if search_button:
-        search_github_and_save_markdown(search_criteria, GITHUB_TOKEN)
+        # Convert the star range to a GitHub API-compatible format
+        stars_parameter = get_stars_parameter(star_range)
+        include_forks = '' if include_forks else '+fork:false'
+        search_github_and_save_markdown(search_criteria, GITHUB_TOKEN, stars_parameter, include_forks)
 
 # Documentation Page
 def documentation():
@@ -221,6 +131,127 @@ def search_results():
     if selected_file:
         selected_file_path = os.path.join("search-results", f"{selected_file}.md")
         st.markdown(open(selected_file_path, 'r', encoding='utf-8').read())
+
+# Function to search GitHub and save markdown
+def search_github_and_save_markdown(search_criteria, github_token, stars_parameter, include_forks):
+    # Initialize variables for pagination
+    per_page = 100  # Maximum items per page for GitHub API
+    page = 1
+    total_repositories = []
+
+    # Add forks parameter to the GitHub API query if include_forks checkbox is checked
+    forks_parameter = '' if include_forks else '+fork:false'
+    query_parameters = stars_parameter + forks_parameter
+
+    # Report the total number of pages for the search criteria
+    total_pages = get_total_pages(search_criteria, per_page, github_token, query_parameters)
+    st.write(f"#### [{total_pages}] Total Pages For '{search_criteria}'")
+
+    if total_pages == 0:
+        st.write("No Repositories Found For The Given Search Criteria.")
+        return
+
+    while page <= total_pages:
+        # Define GitHub API endpoint for repository search
+        api_url = f"https://api.github.com/search/repositories?q={search_criteria}+{query_parameters}&per_page={per_page}&page={page}"
+
+        try:
+            # Send GET request to GitHub API
+            response = requests.get(api_url, headers={"Authorization": f"token {github_token}"})
+
+            response.raise_for_status()  # Raise an exception for any other HTTP error
+
+            # Parse JSON response
+            data = response.json()
+
+            # Extract repositories from current page
+            repositories = data.get('items', [])
+
+            if repositories:
+                total_repositories.extend(repositories)
+                st.write(f"Processed Page {page}/{total_pages}")
+            else:
+                break  # Exit the loop if no more repositories on subsequent pages
+
+            page += 1  # Move to the next page for pagination
+
+            # time.sleep(1)  # Wait for 1 second before making the next request
+        except requests.exceptions.RequestException as e:
+            st.write(f"Error Occurred: {e}")
+            break
+
+    if total_repositories:
+        # Create Markdown content
+        markdown_content = f"---\n"
+        markdown_content += f"### [ {search_criteria} ]\n"
+        markdown_content += f"#### Summary\n"
+        markdown_content += f"###### Date/Timestamp: {current_datetime}\n"  # Add timestamp here
+        markdown_content += f"###### Total Repositories In Your Search: {len(total_repositories)}\n"
+        markdown_content += f"---\n"
+
+        for repo in total_repositories:
+            repo_name = repo.get('full_name', '')
+            repo_url = repo.get('html_url', '')
+            repo_description = repo.get('description', '')
+
+            # Format repository information in Markdown syntax
+            markdown_content += f"##### [{repo_name}]({repo_url})\n"
+            markdown_content += f"###### DESCRIPTION:\n"
+            markdown_content += f"- ###### {repo_description}\n"
+            markdown_content += f"---\n"
+
+        # Save Markdown content to file in the "docs" directory and its subdirectories
+        file_name = f"search-results/{search_criteria}_{current_datetime}.md"
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(markdown_content)
+        st.write(f"Markdown File '{file_name}' With Complete Repository List And Summary Saved Successfully.")
+    else:
+        st.write("No Repositories Found For The Given Search Criteria.")
+
+# Function to get total pages from GitHub API
+def get_total_pages(search_criteria, per_page, github_token, query_parameters):
+    # Define GitHub API endpoint for getting total count of repositories
+    total_count_api_url = f"https://api.github.com/search/repositories?q={search_criteria}+{query_parameters}&per_page=1"
+
+    try:
+        # Send GET request to GitHub API
+        response = requests.get(total_count_api_url, headers={"Authorization": f"token {github_token}"})
+        response.raise_for_status()  # Raise an exception for any HTTP error
+
+        # Parse JSON response
+        data = response.json()
+
+        # Extract total count of repositories
+        total_count = data.get('total_count', 0)
+
+        # Calculate total pages based on total count and items per page
+        total_pages = math.ceil(total_count / per_page)
+
+        return total_pages
+    except requests.exceptions.RequestException as e:
+        st.write(f"Error Occurred While Getting Total Pages: {e}")
+        return 0
+
+# Function to convert star range to GitHub API-compatible format
+def get_stars_parameter(star_range):
+    if star_range == "Most Likes":
+        return '+sort:stars-desc'
+    elif star_range == "0-50 Stars":
+        return '+stars:0..50'
+    elif star_range == "50-100 Stars":
+        return '+stars:50..100'
+    elif star_range == "100-150 Stars":
+        return '+stars:100..150'
+    elif star_range == "150-200 Stars":
+        return '+stars:150..200'
+    elif star_range == "200-250 Stars":
+        return '+stars:200..250'
+    elif star_range == "250-500 Stars":
+        return '+stars:250..500'
+    elif star_range == "Greater Than 500 Stars":
+        return '+stars:>500'
+    else:
+        return ''
 
 # Navigation
 nav_options = {
